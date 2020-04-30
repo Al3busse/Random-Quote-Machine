237a6f102f8023d31bad791627b3e29a36c34470
Alejandro Busse <al3busse@gmail.com>
2020-04-30 05:56:57 -0300

    Updates

---
+++ precache-manifest.42d4da526d56521c089038edfa444609.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3656ad6dc9e3c945d1a7c65ad63cdf8c",
+    "url": "/random-quote-machine/index.html"
+  },
+  {
+    "revision": "6737993acf97b79f27f3",
+    "url": "/random-quote-machine/static/css/2.69121389.chunk.css"
+  },
+  {
+    "revision": "1044c5701d9ec3831ea9",
+    "url": "/random-quote-machine/static/css/main.469a2339.chunk.css"
+  },
+  {
+    "revision": "6737993acf97b79f27f3",
+    "url": "/random-quote-machine/static/js/2.6b509768.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/random-quote-machine/static/js/2.6b509768.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1044c5701d9ec3831ea9",
+    "url": "/random-quote-machine/static/js/main.47c0e955.chunk.js"
+  },
+  {
+    "revision": "21c559728549918444b9",
+    "url": "/random-quote-machine/static/js/runtime-main.d08601ed.js"
+  }
+]);
