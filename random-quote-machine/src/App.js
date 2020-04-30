b1a4805d148bdafd6c764e491a3691e613b92cd1
Alejandro Busse <al3busse@gmail.com>
2020-04-30 03:59:28 -0300

    master

--- random-quote-machine/src/App.js
+++ random-quote-machine/src/App.js
@@ -2,15 +2,24 @@ import React from "react";
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import Quotemachine from "./components/quotemachine/Quotemachine";
+import Container from "react-bootstrap/Container";
 
+const screencenter = {
+  display: "flex",
+  height: "100vh",
+  alignItems: "center",
+  justifyContent: "center",
+};
 class App extends React.Component {
   constructor(props) {
     super(props);
 
     this.state = {
       quotes: [],
-      selectecQuote: null,
+      selectedQuote: [],
     };
+    this.selectRandomQuote = this.selectRandomQuote.bind(this);
+    this.setQuote = this.setQuote.bind(this);
   }
 
   componentDidMount() {
@@ -18,15 +27,39 @@ class App extends React.Component {
       "https://gist.githubusercontent.com/Al3busse/7891ad8844a728eae79cb0d5be696765/raw/303d0453f3bfb205a218f8cd510962c7a950788c/funnyQuotes.json"
     )
       .then((data) => data.json())
-      .then((quotes) => this.setState({ quotes: quotes }));
+      .then((quotes) => this.setState({ quotes }))
+      .then(() =>
+        this.setState({
+          selectedQuote: this.selectRandomQuote(),
+        })
+      );
+  }
+
+  selectRandomQuote() {
+    if (!this.state.quotes.length) {
+      return;
+    }
+    return this.state.quotes[
+      Math.floor(Math.random() * this.state.quotes.length)
+    ];
+  }
+
+  setQuote() {
+    this.setState({
+      selectedQuote: this.selectRandomQuote(),
+    });
   }
 
   render() {
-    console.log(this.state.quotes);
+    console.log(this.state.selectedQuote);
     return (
-      <div id='quote-box'>
-        <Quotemachine />
-      </div>
+      <Container style={screencenter} id='quote-box'>
+        <Quotemachine
+          pickedRandomQuote={this.state.selectedQuote.quote}
+          pickedAuthor={this.state.selectedQuote.author}
+          newQuote={this.setQuote}
+        />
+      </Container>
     );
   }
 }
--- random-quote-machine/src/components/quotemachine/Quotemachine.js
+++ random-quote-machine/src/components/quotemachine/Quotemachine.js
@@ -1,14 +1,37 @@
 import React from "react";
 import Button from "../button/Button.js";
 import ShareSS from "../shareSS/ShareSS";
+import "bootstrap/dist/css/bootstrap.min.css";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+import Card from "react-bootstrap/Card";
 
 const Quotemachine = (props) => (
-  <div>
-    <p id='text'>"{}asda"</p>
-    <p id='author'>asdasd{}</p>
-    <Button buttonName={"New Quote"} />
-    <ShareSS />
-  </div>
+  <Card>
+    <Card.Title style={{ textAlign: "center" }}>
+      Random Quote Machine
+    </Card.Title>
+    <Row>
+      <Col sm={{ span: 10, offset: 1 }} xs={{ span: 10, offset: 1 }}>
+        <p id='text'>
+          {props.pickedRandomQuote ? props.pickedRandomQuote : ""}
+        </p>
+      </Col>
+    </Row>
+    <Row>
+      <Col sm={{ span: 3, offset: 6 }} xs={{ span: 6, offset: 6 }}>
+        <p id='author'>{props.pickedAuthor ? props.pickedAuthor : ""}</p>
+      </Col>
+    </Row>
+    <Row>
+      <Col sm={{ span: 3, offset: 1 }} xs={{ span: 4, offset: 1 }}>
+        <ShareSS />
+      </Col>
+      <Col sm={{ span: 3, offset: 4 }} xs={{ span: 5, offset: 1 }}>
+        <Button buttonName={"New Quote"} clickHandler={props.newQuote} />
+      </Col>
+    </Row>
+  </Card>
 );
 
 export default Quotemachine;
