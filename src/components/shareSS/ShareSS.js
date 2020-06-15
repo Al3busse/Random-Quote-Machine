12cc58528a66ccf3dad0b9f7c7129344bc0e8c35
Alejandro Busse <al3busse@gmail.com>
2020-06-15 00:36:51 -0300

    fixed buttons with anchor for tests

--- src/components/shareSS/ShareSS.js
+++ src/components/shareSS/ShareSS.js
@@ -6,26 +6,26 @@ import "./ShareSS.css";
 
 const ShareSS = (props) => (
   <div>
-    <button
-      className='buttonClass'
-      style={props.borderColor}
-      href={`https://twitter.com/intent/tweet?text=${props.textShare}-${props.authorShare}&hashtags=randomquote`}
-      rel='noopener noreferrer'
-      target='_blank'
-      id='tweet-quote'
-    >
-      <FontAwesomeIcon icon={faTwitter} size='2x'></FontAwesomeIcon>
+    <button className='buttonClass' style={props.borderColor}>
+      <a
+        href={`https://twitter.com/intent/tweet?text=${props.textShare}-${props.authorShare}&hashtags=randomquote`}
+        rel='noopener noreferrer'
+        target='_blank'
+        id='tweet-quote'
+      >
+        <FontAwesomeIcon icon={faTwitter} size='2x'></FontAwesomeIcon>
+      </a>
     </button>
 
-    <button
-      className='buttonClass'
-      style={props.borderColor}
-      href={`https://wa.me/?text=${props.textShare}-${props.authorShare}`}
-      target='_blank'
-      rel='noopener noreferrer'
-      id='whatsapp-quote'
-    >
-      <FontAwesomeIcon icon={faWhatsapp} size='2x'></FontAwesomeIcon>
+    <button className='buttonClass' style={props.borderColor}>
+      <a
+        href={`https://api.whatsapp.com/?text=${props.textShare}-${props.authorShare}`}
+        target='_blank'
+        rel='noopener noreferrer'
+        id='whatsapp-quote'
+      >
+        <FontAwesomeIcon icon={faWhatsapp} size='2x'></FontAwesomeIcon>
+      </a>
     </button>
   </div>
 );
